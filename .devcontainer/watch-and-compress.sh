f37aec8e9ea66af5b5272c27b18ba07b0373a5c5
Kinuthia Ndung'u <kin.ndungu@gmail.com>
2025-07-01 09:15:40 +0000

    Resolve scope bug

--- .devcontainer/watch-and-compress.sh
+++ .devcontainer/watch-and-compress.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 set -e
 
-# Define an array of input and output filenames
-declare -A files=( ["cv.pdf"]="Kinuthia_CV.pdf" ["coverletter.pdf"]="Kinuthia_CoverLetter.pdf" )
+# Define an array of input and output filenames as space-separated pairs
+file_pairs=("cv.pdf:Kinuthia_CV.pdf" "coverletter.pdf:Kinuthia_CoverLetter.pdf")
 
-# Loop over the files and watch each for changes
 # Create a list of all input files
 inputs=()
-for input in "${!files[@]}"; do
+for pair in "${file_pairs[@]}"; do
+    input="${pair%%:*}"
     inputs+=("/workspaces/cv/$input")
 done
 
+# Export file_pairs as a variable for the subshell
+export FILE_PAIRS="${file_pairs[*]}"
+
 # Watch all files in a single entr invocation
 printf "%s\n" "${inputs[@]}" | entr -r bash -c '
-for input in "${!files[@]}"; do
-    output="${files[$input]}"
+for pair in $FILE_PAIRS; do
+    input="/workspaces/cv/${pair%%:*}"
+    output="${pair##*:}"
     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$output" "$input"
 done
 '
